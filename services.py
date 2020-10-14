909be84e2e5339684118bbf5811dd0f8c24bb702
artem <ivashenco45@gmail.com>
2020-10-14 22:12:02 +0300

    [feature] add func get_user_ids

--- services.py
+++ services.py
@@ -7,3 +7,9 @@ def user_exist(user_id):
 
 def get_user_model(user_id):
     return User.select().where(User.id == user_id).get()
+
+
+def get_user_ids():
+    return [user_id for user_id in User.select()]
+
+
